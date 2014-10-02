33604b3e4050f6b313b8e125e4a3c4b7081ff1f3
Oleg19 <oshkinoleg@mail.ru>
2014-10-02 22:36:17 +0400

    Связь таблиц Person и City

---
+++ www/protected/views/person/_view.php
@@ -0,0 +1,21 @@
+<?php
+/* @var $this PersonController */
+/* @var $data Person */
+?>
+
+<div class="view">
+
+	<b><?php echo CHtml::encode($data->getAttributeLabel('id')); ?>:</b>
+	<?php echo CHtml::link(CHtml::encode($data->id), array('view', 'id'=>$data->id)); ?>
+	<br />
+
+	<b><?php echo CHtml::encode($data->getAttributeLabel('FistName')); ?>:</b>
+	<?php echo CHtml::encode($data->FistName); ?>
+	<br />
+
+	<b><?php echo CHtml::encode($data->getAttributeLabel('LastName')); ?>:</b>
+	<?php echo CHtml::encode($data->LastName); ?>
+	<br />
+
+
+</div>
