92d0567f802b1819597053e7435abb1ea599e6e6
Oleg19 <oshkinoleg@mail.ru>
2014-10-05 22:31:53 +0400

    Revert "Revert "Связь таблиц Person и City""
    This reverts commit 3da01f6977e578283e2e3aaf44e824cea61edcbd.

---
+++ www/protected/views/person/_search.php
@@ -0,0 +1,35 @@
+<?php
+/* @var $this PersonController */
+/* @var $model Person */
+/* @var $form CActiveForm */
+?>
+
+<div class="wide form">
+
+<?php $form=$this->beginWidget('CActiveForm', array(
+	'action'=>Yii::app()->createUrl($this->route),
+	'method'=>'get',
+)); ?>
+
+	<div class="row">
+		<?php echo $form->label($model,'id'); ?>
+		<?php echo $form->textField($model,'id',array('size'=>10,'maxlength'=>10)); ?>
+	</div>
+
+	<div class="row">
+		<?php echo $form->label($model,'FistName'); ?>
+		<?php echo $form->textField($model,'FistName',array('size'=>10,'maxlength'=>10)); ?>
+	</div>
+
+	<div class="row">
+		<?php echo $form->label($model,'LastName'); ?>
+		<?php echo $form->textField($model,'LastName',array('size'=>10,'maxlength'=>10)); ?>
+	</div>
+
+	<div class="row buttons">
+		<?php echo CHtml::submitButton('Search'); ?>
+	</div>
+
+<?php $this->endWidget(); ?>
+
+</div><!-- search-form -->
